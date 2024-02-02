050c6b8548612c97061442c57f93394dc3c243fe
emnesty <legostr@gmail.com>
2024-02-02 12:31:12 -0300

    ajustes

--- app/components/ui/card-hover-effect.tsx
+++ app/components/ui/card-hover-effect.tsx
@@ -17,7 +17,7 @@ export const HoverEffect = ({
   let [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
 
   return (
-    <div className="h-[50rem] flex flex-col py-4 md:pt-[12rem] sm:pt-[47rem] max-[629px]:pt-[50rem] max-[422px]:pt-[62rem] min-[450px]:pt-[62rem] max-[350px]:pt-[65rem] antialiased bg-gray-900 dark:bg-gray-900 dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
+    <div className="h-[50rem] flex flex-col py-4 md:pt-[12rem] sm:pt-[47rem] min-[629px]:pt-[50rem] min-[422px]:pt-[62rem] min-[450px]:pt-[62rem] min-[350px]:pt-[70rem] max-[349px]:pt-[70rem]  antialiased bg-gray-900 dark:bg-gray-900 dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
       <div
         className="mx-auto max-w-2xl lg:text-center"
         data-aos="fade-up"
